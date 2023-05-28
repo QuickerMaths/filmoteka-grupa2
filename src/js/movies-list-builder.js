e31b53750596e017cebac4561e0c538e4d9f9334
QuickerMaths <mateusz.szalowicz@gmail.com>
2023-05-28 12:33:39 +0200

    commit

--- src/js/movies-list-builder.js
+++ src/js/movies-list-builder.js
@@ -1,13 +1,16 @@
 import { genreToString } from './genres';
+import noImageFound from '../images/no-image.png';
 
 export const listBuilder = moviesArray => {
   return moviesArray
     .map(item => {
       return `
       <li class="cover__container" id='${item.id}'>
-      <img class="cover__image" src="https://image.tmdb.org/t/p/w500${item.poster_path}" alt="${
-        item.title
-      }"/>
+      <img class="cover__image" src="${
+        item.poster_path === null
+          ? noImageFound
+          : `https://image.tmdb.org/t/p/w500${item.poster_path}`
+      }" alt="${item.title}/>
         <div class="cover__info">
           <p class="cover__figcaption">
             <b>${item.title}</b>
