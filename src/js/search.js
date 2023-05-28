88a43689f377b369da6c42dd028b9847255ed0e7
Łukasz Kocela <lukaszkocela@gmail.com>
2023-05-28 08:16:49 +0200

    fixed search with loader

--- src/js/search.js
+++ src/js/search.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 import { listBuilder } from './movies-list-builder';
 import { pagination } from './pagination';
+import { displayLoading, hideLoading } from './loader';
 
 const API_KEY = 'eaafeda4857b9c9fecdb45e75f22375a';
 const API_URL = 'https://api.themoviedb.org/3';
@@ -13,7 +14,7 @@ let queryParam = '';
 
 export const searchByKeyword = async query => {
   const noResults = document.querySelector('.header__error-message');
-
+  displayLoading();
   try {
     const response = await axios.get(`${API_URL}/search/movie?api_key=${API_KEY}&query=${query}`);
     if (response.data.results.length === 0) {
@@ -25,13 +26,14 @@ export const searchByKeyword = async query => {
   } catch (err) {
     console.log(err);
   }
+  hideLoading();
 };
 
-searchForm.addEventListener('submit', event => {
+searchForm.addEventListener('input', event => {
   event.preventDefault();
 
-  queryParam = searchInput.value;
-  const query = searchInput.value;
+  queryParam = searchInput.value.trim();
+  const query = searchInput.value.trim();
   if (query === '') return;
   searchByKeyword(query);
 });
