66257cfa3d72ff670d2b672a51d2039ac1483a6b
KrzysztofPak <117757357+KrzysztofPak@users.noreply.github.com>
2023-05-29 18:23:11 +0200

    Update team-load-to-modal.js

--- src/js/team-load-to-modal.js
+++ src/js/team-load-to-modal.js
@@ -1,49 +1,53 @@
-document.getElementById("modal-link").addEventListener("click", function(event) {
-  event.preventDefault(); });
-
 const refs = {
-    openModal: document.querySelector('.open-modal-team'),
-    closeModal: document.querySelector('.close-modal-team'),
-    teamBackdrop: document.querySelector('.backdrop-modal'),
-    teamModal: document.getElementsByClassName('team__modal'),
-  };
-  
-  refs.openModal.addEventListener('click', openModalTeam);
-  refs.closeModal.addEventListener('click', closeModalTeam);
-  
-  function openModalTeam(event) {
-    refs.teamBackdrop.classList.remove('team__backdrop--hidden');
-    document.addEventListener('keydown', onEscapeClose);
-    document.addEventListener('click', onBackdropClose);
-    refs.teamModal[0].classList.add('openModalAnimationTeam');
-  }
-  
-  function closeModalTeam(event) {
-    refs.teamModal[0].classList.remove('closeModalAnimationTeam');
-    refs.teamBackdrop.classList.add('team__backdrop--hidden');
-    document.removeEventListener('keydown', onEscapeClose);
-    document.body.style.overflow = '';
-  }
-  
-  function onEscapeClose(event) {
-    if (event.code === 'Escape') {
-      refs.teamModal[0].classList.remove('openModalAnimationTeam');
-      refs.teamModal[0].classList.add('closeModalAnimationTeam');
-      setTimeout(() => {
-        closeModalTeam();
-      }, 400);
+  openModal: document.querySelector('.open-modal-team'),
+  closeModal: document.querySelector('.close-modal-team'),
+  teamBackdrop: document.querySelector('.backdrop-modal'),
+  teamModal: document.getElementsByClassName('team__modal'),
+};
+
+refs.openModal.addEventListener('click', openModalInPopupWindow);
+refs.closeModal.addEventListener('click', closeModalTeam);
+
+function openModalInPopupWindow(event) {
+  event.preventDefault();
+  const modalURL = './src/partials/team-modal.html';
+  const windowFeatures = 'width=800,height=600,scrollbars=yes,resizable=yes';
+  window.open(modalURL, 'modalPopup', windowFeatures);
+}
+
+function openModalTeam(event) {
+  refs.teamBackdrop.classList.remove('team__backdrop--hidden');
+  document.addEventListener('keydown', onEscapeClose);
+  document.addEventListener('click', onBackdropClose);
+  refs.teamModal[0].classList.add('openModalAnimationTeam');
+}
+
+function closeModalTeam(event) {
+  refs.teamModal[0].classList.remove('closeModalAnimationTeam');
+  refs.teamBackdrop.classList.add('team__backdrop--hidden');
+  document.removeEventListener('keydown', onEscapeClose);
+  document.body.style.overflow = '';
+}
+
+function onEscapeClose(event) {
+  if (event.code === 'Escape') {
+    refs.teamModal[0].classList.remove('openModalAnimationTeam');
+    refs.teamModal[0].classList.add('closeModalAnimationTeam');
+    setTimeout(() => {
       closeModalTeam();
-    }
+    }, 400);
+    closeModalTeam();
   }
-  
-  function onBackdropClose(event) {
-    if (event.target === refs.teamBackdrop) {
-      refs.teamModal[0].classList.remove('openModalAnimationTeam');
-      refs.teamModal[0].classList.add('closeModalAnimationTeam');
-      setTimeout(() => {
-        closeModalTeam();
-      }, 400);
-    }
+}
+
+function onBackdropClose(event) {
+  if (event.target === refs.teamBackdrop) {
+    refs.teamModal[0].classList.remove('openModalAnimationTeam');
+    refs.teamModal[0].classList.add('closeModalAnimationTeam');
+    setTimeout(() => {
+      closeModalTeam();
+    }, 400);
   }
+}
   
   
