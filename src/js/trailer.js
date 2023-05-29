d2841b4585df6b87242d71ccc4afe3a8518a82f3
KrzysztofPak <117757357+KrzysztofPak@users.noreply.github.com>
2023-05-29 18:36:14 +0200

    Update trailer.js

--- src/js/trailer.js
+++ src/js/trailer.js
@@ -5,6 +5,7 @@ import { filmId } from './loading-into-trailer';
 
 const srcTrailer = 'https://www.youtube.com/embed/';
 const trailerBtn = document.querySelector('.trailer-btn');
+const closeModalBtn = document.querySelector('.close-modal__trailer');
 
 const closeModalHandler = e => {
   if (e.code === 'Escape') {
@@ -17,30 +18,30 @@ trailerBtn.addEventListener('click', () => {
   openTrailer(filmId);
 });
 
-const modal = basicLightbox.create(`
-  <iframe class="iframe-container" width="900" height="600" src="" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-<button class="close-modal__trailer">
-     </button>`);
-const iframeTrailer = modal.element().querySelector('iframe');
+closeModalBtn.addEventListener('click', () => {
+  closeModalHandler({ code: 'Escape' });
+});
 
-const modal2 = basicLightbox.create(`
-    <iframe class="iframe-container" src="${srcTrailer}6DhiiFGk4_s" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-      `);
+const modal = basicLightbox.create(`
+  <div class="modal-content">
+    <iframe class="iframe-container" src="" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+    <button class="close-modal__trailer"></button>
+  </div>
+`);
+const iframeTrailer = modal.element().querySelector('iframe');
 
 export default function openTrailer(id) {
   fetchTrailerById(id)
     .then(data => {
       const key = data.results[0].key;
-
-      iframeTrailer.src = srcTrailer + key;
+      const trailerURL = srcTrailer + key;
+      
+      iframeTrailer.src = trailerURL;
       modal.show();
 
-      const closeBtn = document.querySelector('.close-modal__trailer');
-      closeBtn.addEventListener('click', closeModalHandler);
       window.addEventListener('keydown', closeModalHandler);
     })
     .catch(error => {
-      modal2.show();
       console.log(error);
     });
 }
